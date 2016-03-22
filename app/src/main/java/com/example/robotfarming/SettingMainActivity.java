83d01309ae903ef2af20012612f9132838934ab5
Rui Geng <gengr.r@gmail.com>
2016-03-21 20:54:46 -0400

    add a main setting view

---
+++ app/src/main/java/com/example/robotfarming/SettingMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.robotfarming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by 瑞 on 3/21/2016.
+ */
+public class SettingMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settingativity_main);
+    }
+}
